770e64f5fe35f00e86f10598297b5d46a8415aa0
GangaMegha <gangamegha29@gmail.com>
2016-10-26 03:42:21 +0530

    prediction attempt successful

--- read_images.py
+++ read_images.py
@@ -2,6 +2,7 @@ import time
 import os
 import glob
 import tensorflow as tf
+import numpy as np
 
 # path = 'by_class'
 path = 'test'
@@ -18,7 +19,8 @@ try:
 except:
     file_labels = [str(unichr(i)) for i in file_classes]    #python 2.7    
 
-
+# file_labels=np.array(file_labels)
+# tf.convert_to_tensor(file_labels.eval())
 t3 = time.time()
 print('Time to list labels: ', t3-t2)
 
@@ -37,7 +39,7 @@ coord = tf.train.Coordinator()
 threads = tf.train.start_queue_runners(coord=coord, sess=sess)
 
 for i in range(len(file_classes)): #length of your filename list
-  image = my_img.eval(session = sess) #here is your image Tensor :)
+  image = my_img.eval(session = sess).ravel() #here is your image Tensor :)
 
 coord.request_stop()
 coord.join(threads)
@@ -45,3 +47,30 @@ t4 = time.time()
 print('Time to read images: ',t4-t3)
 # Takes about  seconds to read test folder on my 4GB PC :-D
 # And the code works!!
+#Input the images from mnist to tensorflow
+
+file_labels = np.zeros((len(file_labels),10))
+
+x = tf.placeholder(tf.float32, [None, 128*128*3],name="x")
+W = tf.Variable(tf.zeros([128*128*3, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+init = tf.initialize_all_variables()
+
+sess = tf.Session()
+sess.run(init)
+
+#Training the NN
+sess.run(train_step, feed_dict={x: image.reshape(1,image.shape[0]), y_: file_labels})
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(correct_prediction)
+# print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
