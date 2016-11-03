48b309da0ba835c52394ccd35976a735e44380b2
GangaMegha <gangamegha29@gmail.com>
2016-11-04 02:53:17 +0530

    Crops to contours

--- compress_resize.py
+++ compress_resize.py
@@ -1,8 +1,9 @@
 import os
 import glob
 import cv2
+import numpy as np
 
-path = 'test'
+path = 'by_class'
 new_path = 'resized'
 
 file_names=glob.glob(os.path.join(path,'*','train_*','*.png'))
@@ -12,7 +13,7 @@ unique_folders = glob.glob(os.path.join(path,'*','train_*/'))
 
 if not os.path.isdir(new_path):
     os.mkdir(new_path)
-
+# print(file_names,'yolo')
 for ele in unique_folders:
     j,k,l = (ele.split('/')[1:])
     new_loc = os.path.join(new_path,j,k,l)
@@ -21,11 +22,21 @@ for ele in unique_folders:
         os.mkdir(new_loc)
 
 for i in range(no_of_files):
-    temp = cv2.resize(cv2.imread(file_names[i],0), (28,28))
-    j,k,l = (file_names[i].split('/')[1:])
-    new_loc = os.path.join(new_path,j,k,l)
-    print(file_names[i], new_loc)
-    # cv2.imshow(new_loc, temp)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-    cv2.imwrite(new_loc, temp)
+    img = cv2.imread(file_names[i],0)
+    img_=img.copy()
+    im2,contours,hier=cv2.findContours(img,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+    areas=[cv2.contourArea(c) for c in contours]
+    areas=filter(lambda x: x<0.9*img.shape[0]*img.shape[1],areas)
+    if(len(areas)!=0):
+        idx=np.argmax(areas)
+        x,y,w,h=cv2.boundingRect(contours[idx])
+        im=img_[y:y+h,x:x+w]
+        im2=cv2.resize(im,(28,28))
+        j,k,l = (file_names[i].split('/')[1:])
+        new_loc = os.path.join(new_path,j,k,l)
+        print(file_names[i], new_loc)
+        # cv2.imshow(new_loc, temp)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        cv2.imwrite(new_loc, im2)
+
