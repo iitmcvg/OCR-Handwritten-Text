1f812021565afea75efb60099e81750e2479c7a0
Rag1 <sraghavan02@gmail.com>
2016-11-06 01:05:31 +0530

    CSV file reading and writing output file given text file are the  added updates

--- Textsegmentation/charsegment.cpp
+++ Textsegmentation/charsegment.cpp
@@ -10,6 +10,10 @@ struct data
 {
   int img[28][28];
 } s1;
+struct CONT
+{
+  int topx,topy,width,height;
+}cntr;
 int main(int argc,char *argv[])
 {
   if(argc!=2) exit(0);
@@ -20,10 +24,10 @@ int main(int argc,char *argv[])
   Mat img4=imread(argv[1]);
   Mat img(600,800,CV_8UC3);Mat img1(600,800,CV_8UC1);
   resize(img4,img,img.size());
-  imshow("dsas",img);
-  waitKey(0);
+  // imshow("dsas",img);
+  // waitKey(0);
  cvtColor(img,img,CV_BGR2GRAY);
-// Mat img(img2);
+ // Mat img(img2);
   // Rect rec(20,20,img2.size().width/4,img2.size().height/4);
   // Mat img3=img2(rec);
   // resize(img3,img,img.size());
@@ -32,8 +36,8 @@ int main(int argc,char *argv[])
   threshold(img, img, 0, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
   dilate(img, img, Mat());
   erode(img,img,Mat());
-  imshow("dsas",img);
-  waitKey(0);
+  // imshow("dsas",img);
+  // waitKey(0);
   Mat mserOutMask(img.size(),CV_8UC1,Scalar(0));
   Ptr<MSER> mserExtractor  = MSER::create();
 
@@ -54,7 +58,7 @@ int main(int argc,char *argv[])
     vector<vector<Point> > contours;
     vector<Vec4i> hierarchy;
     threshold(mserOutMask, threshold_output, 100, 255, THRESH_BINARY );
-  imshow(" ",threshold_output);
+  // imshow(" ",threshold_output);
     findContours( threshold_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) );
     vector<vector<Point> > contours_poly( contours.size() );
     vector<Rect> boundRect( contours.size() );
@@ -107,10 +111,10 @@ for(size_t j=i+1;j<contours.size();j++)
 // }
 for(size_t i=0;i<contours.size();i++)
 {
-  if(boundRect[i].height<avght/3)
+  if(boundRect[i].height<avght/2)
   boundpres[i]=1;
 }
-ofstream f("segments.dat",ios::binary);
+ofstream f("charboxes.csv");
 Mat m1;
 for(size_t j=0;j<contours.size();j++)
 {
@@ -127,23 +131,114 @@ for(size_t j=0;j<contours.size();j++)
     }
 //erode(m1,m1,Mat());
 //erode(m1,m1,Mat());
-//threshold(m1, m1, 160, 255, THRESH_BINARY );
+threshold(m1, m1, 100, 255, THRESH_BINARY );
 //  cout<<m1.rows<<" "<<m1.cols<<endl;
- //  imshow("bbox",m1);
- // waitKey(0);
+  // imshow("bbox",m1);
+  // waitKey(0);
+
 for(int x=0;x<28;x++)
-for(int y=0;y<28;y++)
-s1.img[y][x]=m1.at<uchar>(y,x);
-f.write((char*)&s1,sizeof(s1));
+{
+  for(int y=0;y<28;y++)
+{
+  s1.img[y][x]=m1.at<uchar>(y,x);
+  f<<s1.img[y][x]<<",";
+}
+  f<<"\n";
+}
+//f<<"\n";
 }}
 f.close();
+int contrsize=0;
 for( size_t i = 0; i < contours.size(); i++ )
    {
      if(boundpres[i]==0)
-     rectangle(img,boundRect[i],Scalar(0,0,0),1,8,0);
-   }
+     {
+       rectangle(img,boundRect[i],Scalar(0,0,0),1,8,0);
+       contrsize++;
+     }
+    }
 
-  imshow("mser", img);
-waitKey(0);
+// imshow("mser", img);
+// waitKey(0);
+
+//call python file
+float avgdis=0.0,dis,counter=0;
+for(size_t i=0;i<contours.size();i++)
+{
+  if(boundpres[i]==0)
+  {
+    int j;
+    for(j=i+1;j<contours.size();j++)
+      if(boundpres[j]==0)
+        break;
+    if(j==contours.size())
+    {
+      break;
+    }
+    if(centers[j].y-centers[i].y>avght/2)
+      {
+        i=j-1;
+        continue;
+      }
+    else
+    {
+      dis=boundRect[j].x-boundRect[i].width-boundRect[i].x;
+      avgdis=(avgdis*counter+dis)*1.0/(counter+1);
+      counter++;
+      i=j-1;
+    }
+  }
+}
+//cout<<"Avgdis "<<avgdis<<"\n";
+ifstream filein("prediction.txt");
+ofstream fileout("output.txt");
+size_t i;
+for(i=0;i<contours.size()&&!filein.eof();i++)
+{
+  if(boundpres[i]==0)
+  {
+    char c;
+    filein.get(c);
+    // Mat m1=img(boundRect[i]);
+    // imshow("wind",m1);
+    // cout<<c<<endl;
+    // waitKey(0);
+    int j;
+    for(j=i+1;j<contours.size();j++)
+      if(boundpres[j]==0)
+        break;
+    if(j==contours.size())
+    {
+      fileout<<c;
+      break;
+    }
+    if(centers[j].y-centers[i].y>avght/2)
+      {
+        fileout<<c;
+        fileout<<"\n";
+        i=j-1;
+        continue;
+      }
+    else
+    {
+      if(boundRect[j].x-boundRect[i].width-boundRect[i].x>avgdis*1.5)
+        {
+          fileout<<c;
+          fileout<<" ";
+          i=j-1;
+          continue;
+        }
+      else
+      {
+        fileout<<c;
+        i=j-1;
+        continue;
+      }
+    }
+  }
+}
+//cout<<i-contours.size()<<" "<<contours.size()<<" "<<contrsize<<endl;
+filein.close();
+fileout.close();
   return 0;
 }
